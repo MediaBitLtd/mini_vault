896852ba04c9042ea9433cceb5995a5c15926907
Joao Matos <joao.matos@media-bit.co.uk>
2025-02-18 20:55:58 +0000

    MV-3: Fixes to the login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,13 +1,42 @@
 <template>
-    <div class="w-full">
+    <div class="w-full md:min-w-72">
         <h3 class="text-gray-800 text-3xl text-center font-extrabold mb-8 dark:text-zinc-200">
             Mini Vault
         </h3>
 
         <div class="flex flex-col gap-4">
-            <InputText v-model="form.email" name="email" type="email" autocomplete="ds" required placeholder="Email" />
+            <div>
+                <InputText
+                    v-model="form.email"
+                    autofocus
+                    name="email"
+                    type="email"
+                    autocomplete="ds"
+                    required
+                    placeholder="Email"
+                    :invalid="!!form.errors.email"
+                    @keydown.enter.prevent="login"
+                    fluid
+                />
+                <Message v-if="form.errors.email" severity="error" size="small" variant="simple">
+                    {{ form.errors.email }}
+                </Message>
+            </div>
 
-            <Password v-model="form.password" :feedback="false" toggleMask placeholder="Password" />
+            <div>
+                <Password
+                    v-model="form.password"
+                    :feedback="false"
+                    toggle-mask
+                    placeholder="Password"
+                    :invalid="!!form.errors.email"
+                    @keydown.enter.prevent="login"
+                    fluid
+                />
+                <Message v-if="form.errors.password" severity="error" size="small" variant="simple">
+                    {{ form.errors.password }}
+                </Message>
+            </div>
         </div>
 
         <div class="mt-8">
@@ -18,7 +47,7 @@
 <script setup lang="ts">
 import AuthLayout from '~/Layouts/AuthLayout.vue'
 import { useForm, usePage } from '@inertiajs/vue3'
-import { InputText, Password, Button } from 'primevue'
+import { InputText, Password, Message, Button } from 'primevue'
 import CookieJS from 'js-cookie'
 import axios from 'axios'
 
@@ -27,6 +56,7 @@ const form = useForm({
     email: '',
     password: '',
 })
+
 
 CookieJS.remove('_accessToken')
 
