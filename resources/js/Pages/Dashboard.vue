33aaf6a16651375d6a1478332410c909b07e4961
Joao Matos <joao.matos@media-bit.co.uk>
2025-02-19 18:39:25 +0000

    Playing around with dashboard ideas

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,35 @@
 <template>
-    <h1>Dashboard</h1>
+    <h1 class="text-3xl font-bold mb-10">Dashboard</h1>
+    <div class="flex flex-col gap-4">
+        <Card>
+            <template #title>Authenticator</template>
+            <template #content>
+                <ul>
+                    <li>Favourite #1</li>
+                    <li>Favourite #2</li>
+                </ul>
+            </template>
+        </Card>
+        <Card>
+            <template #title>Items</template>
+            <template #content>
+                <ul>
+                    <li>Favourite #1</li>
+                    <li>Favourite #2</li>
+                </ul>
+            </template>
+        </Card>
+        <Card>
+            <template #title>Add new record</template>
+            <template #content>
+                <ul>
+                    <li>Favourite #1</li>
+                    <li>Favourite #2</li>
+                </ul>
+            </template>
+        </Card>
+    </div>
 </template>
 <script setup lang="ts">
-
+import { Card } from 'primevue'
 </script>
