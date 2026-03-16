51bfea11c50ce78f6f456720057946bb0e0e4246
Joao Matos <joao.matos@media-bit.co.uk>
2026-03-16 13:07:35 +0000

    Fixed old oauth ids not working on route resolution;

--- app/OAuth/Client.php
+++ app/OAuth/Client.php
@@ -14,6 +14,11 @@ class Client extends BaseClient
 {
     protected $hidden = [];
 
+    protected function isValidUniqueId($value): bool
+    {
+        return is_string($value) && $value !== '';
+    }
+
     public function skipsAuthorization(Authenticatable $user, array $scopes): bool
     {
         return Str::contains($this->redirect, Config::get('app.url'));
